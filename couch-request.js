8fb73b9d86d3e360568e2962846628e5f0be4ebe
Peter Koraca <peter.koraca@gmail.com>
2013-08-24 17:16:38 +0100

    Wrapper created

--- couch-request.js
+++ couch-request.js
@@ -1,9 +1,108 @@
 var request = require('request'),
-	util = require('util');
+	pathModule = require('path');
+
+
+function couchrequest( newOptions ){
+
+	if ( newOptions && typeof newOptions == "object" && 
+		 newOptions.databaseUrl && newOptions.databaseUrl.length > 0 ) {		
+		
+		var options = {
+			method: "GET",
+			url: newOptions.databaseUrl,
+			headers: {"Content-Type": "application/json"}
+		};
+		
+
 	
-function couchrequest(  ){
-	
-	
+		var reqFunction = function(){
+			// GET //////////////////////////////////////////////////////////////////////
+			if ( arguments.length == 2 ){			
+				var path = arguments[0];
+				var callback = arguments[1];	
+				
+				options.url += "/" + path;				
+
+				// Make the request
+				request( options, function(err, res, body){
+														
+					if( err ){
+						callback( err, null );
+					} else{
+						var parseerr = false;
+						var result;
+						try{
+							result = JSON.parse( body );
+						} catch(e){
+							parseerr = true;
+						}
+						if( parseerr ){
+							callback( new Error("The response is not JSON - check the URL"), null );
+						}else{
+							if( result.error ){
+								callback( result, null );
+							}else{
+								// ALL GOOD
+								callback( null, result );								
+							}
+						}
+					}
+				});
+
+
+			// POST //////////////////////////////////////////////////////////////////////
+			} else if ( arguments.length == 3 ){
+				var path = arguments[0];
+				var theObject = arguments[1];
+				var callback = arguments[2];
+				
+				if( theObject && typeof theObject == "object" ){
+					var encodedObject = JSON.stringify( theObject );
+					options.method = "POST";					
+					options.body = encodedObject;
+					
+					// Make the request
+					request( options, function(err, res, body){
+															
+						if( err ){
+							callback( err, null );
+						} else{
+							var parseerr = false;
+							var result;
+							try{
+								result = JSON.parse( body );
+							} catch(e){
+								parseerr = true;
+							}
+							if( parseerr ){
+								callback( new Error("The response is not JSON - check the URL"), null );
+							}else{
+								if( result.error ){
+									callback( result, null );
+								}else{
+									// ALL GOOD
+									callback( null, result );								
+								}
+							}
+						}
+					});
+					
+					
+				} else{
+					callback( new Error("Passed second argument is not a valid JSON object"), null);
+				}				
+				
+			}		
+		};
+		
+		return reqFunction;
+		
+		
+	} else {
+		return null;
+	}
 };
 
-modules.exports = couchrequest;
+
+// EXPORTS
+module.exports = couchrequest;
---
+++ test.js
@@ -0,0 +1,19 @@
+var couchrequest = require('./couch-request'),
+	util = require('util');
+	
+
+
+var database = couchrequest( {databaseUrl: "http://127.0.0.1:5984/reqtest" } );
+
+
+// Use
+
+//database("_all_docs", "burek", function(err, data){
+//database("", {name: "peter"}, function(err, data){
+database("_all_docs", function(err, data){
+	if( err ) {
+		console.log( err );
+	} else{
+		console.log( util.inspect( data, {colors: true} ) );
+	}
+});
